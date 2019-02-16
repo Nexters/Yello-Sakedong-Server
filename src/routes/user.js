df59271e1c90785260fdec4971c04549a755fc0b
KimHunJin <ysg01129@naver.com>
2019-02-16 19:40:05 +0900

    병합

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,5 @@ const userController = require('../controllers/user');
 const checkAuth = require('../middleware/check-auth');
 
 router.post("/signup", userController.user_signup);
-router.post("/user", userController.user_post);
 
 module.exports = router;
