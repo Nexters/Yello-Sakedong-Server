a27928998faface1268974796af5d4fe963f4406
jaehwaii <ypmc91@gmail.com>
2019-02-16 16:32:53 +0900

    other API basic setting & user_signup doing

--- src/db/food.js
+++ src/db/food.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose');
+
+const foodSchema = mongoose.Schema({
+    _id : String,
+    foodName : String,
+    foodOriginName : String,
+    foodEnglishName : String,
+    foodRegion : String,
+    foodImageUrl : String,
+    foodComment : String
+
+});
+
+module.exports = mongoose.model('Food', foodSchema);
--- src/db/like.js
+++ src/db/like.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose');
+
+const likeSchema = mongoose.Schema({
+    _id : String,
+
+
+});
+
+module.exports = mongoose.model('Like', likeSchema);
