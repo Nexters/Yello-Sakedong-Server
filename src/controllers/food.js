beb1a5910709de54b3b6d00ad812be7c7ff70f33
jaehwaii <ypmc91@gmail.com>
2019-02-19 01:05:12 +0900

    add get foodList & add multer

--- src/controllers/food.js
+++ src/controllers/food.js
@@ -3,6 +3,30 @@ const mongoose = require('mongoose');
 const User = require('../db/user');
 const Food = require('../db/food');
 
+exports.food_list = (req, res, next) =>{
+    Food.find()
+    .then(docs =>{
+        const response = {
+            FoodList: docs.map(doc =>{
+                return {
+                    _id : doc._id,
+                    foodName : doc.foodName,
+                    foodImageUrl : doc.foodImageUrl,
+                    foodComment : doc.foodComment,
+                    foodEmoji : doc.foodEmoji
+                }
+            })
+        }
+        res.status(200).json(response);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error : err
+        });
+    })    
+};
+
 exports.food_add = (req, res, next) => {
     const food = new Food({
         _id : new mongoose.Types.ObjectId(),
--- src/db/food.js
+++ src/db/food.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const foodSchema = mongoose.Schema({
     _id : String,
-    foodName : String,
+    foodName : {type: String, required : true},
     foodOriginName : String,
     foodEnglishName : String,
     foodRegion : String,
-    foodImageUrl : String,
-    foodComment : String,
+    foodImageUrl : {type: String, required : true},
+    foodComment : {type : String, required : true},
     foodEmoji : {
+        required : true,
         type : Number,
         enum : ['감동', '매움', '토', '씀', '윙크', '천국', '띠용', '분노','허걱','뀨']
     }
